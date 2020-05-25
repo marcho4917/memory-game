e28d5af29f871ed0fd9d1f47977664305d7e4d31
Martyna Chojnowska <martynachojnowska12@gmail.com>
2020-05-25 18:13:27 +0200

    init new project

---
+++ memoryGame.js
@@ -0,0 +1,89 @@
+const memoryCards = document.querySelectorAll('.memory-card');
+const endGame = document.querySelector('.end-text');
+const newGame = document.querySelector('button');
+
+let pairsAmount = 6;
+let hasReverseMemoryCard = false;
+let firstMemoryCard, secondMemoryCard;
+let lockTable = false;
+
+
+function reverseCard() {
+    if (lockTable) return;
+    if(this === firstMemoryCard) return;
+
+    this.classList.add('reverse');
+
+    if(!hasReverseMemoryCard) {
+        hasReverseMemoryCard = true;
+        firstMemoryCard = this;
+
+        return;
+    } 
+    secondMemoryCard = this;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstMemoryCard.dataset.name === secondMemoryCard.dataset.name;
+    
+    isMatch ? disableMemoryCards() : unreverseCards();
+}
+
+function disableMemoryCards() {
+    firstMemoryCard.removeEventListener('click', reverseCard);
+    secondMemoryCard.removeEventListener('click', reverseCard);
+    lockTable = true;
+    setTimeout(() => {
+        firstMemoryCard.classList.add('off');
+        secondMemoryCard.classList.add('off');
+        resetTable();
+    }, 1500);
+
+    pairsAmount--;
+    pairsAmount === 0 ? 
+    setTimeout(() => { endGame.classList.toggle('on')}, 1500) : null;
+        
+    newGame.addEventListener('click', startNewGame);
+}
+
+function unreverseCards() {
+    lockTable = true;
+
+    setTimeout(() => {
+        firstMemoryCard.classList.remove('reverse');
+        secondMemoryCard.classList.remove('reverse');
+        resetTable();
+    }, 1000);
+}
+
+function resetTable() {
+    [hasReverseMemoryCard, lockTable] = [false, false];
+    [firstMemoryCard, secondMemoryCard] = [null, null];
+}
+
+function startNewGame() {
+    endGame.classList.toggle('on');
+    pairsAmount = 0;
+    memoryCards.forEach(memoryCard => {
+        memoryCard.classList.remove('reverse', 'off');
+    });
+
+    shuffleMemoryCards();
+
+    memoryCards.forEach(memoryCard => 
+        memoryCard.addEventListener('click', reverseCard));
+}
+
+function shuffleMemoryCards() {
+    memoryCards.forEach(memoryCard => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        memoryCard.style.order = randomPosition;
+    });
+};
+
+memoryCards.forEach(memoryCard => 
+    memoryCard.addEventListener('click', reverseCard));
+
+shuffleMemoryCards();
